71ad6baa8828c7424591bb7258682e5393a77972
Muhammad Akmal <makmal316@gmail.com>
2025-05-12 18:06:27 +0700

    fix: add registration and schedule after create student seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,10 +3,13 @@
 namespace Database\Seeders;
 
 use App\Models\Jadwal;
+use App\Models\Pendaftaran;
+use App\Models\PendaftaranJadwal;
 use App\Models\Siswa;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -45,6 +48,29 @@ class DatabaseSeeder extends Seeder
 
         Jadwal::insert($schedules);
 
-        Siswa::factory(10)->create();
+        Siswa::factory(10)->create()->each(function ($siswa) {
+            $prefix = 'PN';
+            $today = Carbon::now()->format('dmY');
+            $randomNumber = str_pad(mt_rand(1, 999), 3, '0', STR_PAD_LEFT);
+            $idPendaftaran = $prefix . $today . $randomNumber;
+
+            Pendaftaran::create([
+                'id' => $idPendaftaran,
+                'id_siswa' => $siswa->id,
+                'metode_pembayaran' => fake()->randomElement(['Dana', 'Transfer', 'QRIS']),
+                'status' => fake()->randomElement(['Pending', 'Diterima', 'Ditolak']),
+                'tanggal' => now(),
+                'info' => fake()->randomElement(['Teman', 'Media Sosial', 'Brosur', 'Lainnya']),
+            ]);
+
+            $jadwalIds = Jadwal::inRandomOrder()->limit(2)->pluck('id'); // ambil 2 random jadwal
+
+            foreach ($jadwalIds as $jadwalId) {
+                PendaftaranJadwal::create([
+                    'id_pendaftaran' => $idPendaftaran,
+                    'id_jadwal' => $jadwalId,
+                ]);
+            }
+        });
     }
 }
